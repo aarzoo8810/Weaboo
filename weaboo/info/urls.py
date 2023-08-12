9c635e82f59905d54acc83bcf1a3801e20d67bc3
Aarzoo <r.zoo786md@gmail.com>
2023-08-12 21:53:48 +0530

    adds url name

--- weaboo/info/urls.py
+++ weaboo/info/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index),
-    path("anime/<int:mal_anime_id>", views.anime_info),
-    path("manga/<int:mal_manga_id>", views.manga_info),
+    path("anime/<int:mal_anime_id>", views.anime_info, name='anime-details'),
+    path("manga/<int:mal_manga_id>", views.manga_info, name='manga-details'),
 ]
 
